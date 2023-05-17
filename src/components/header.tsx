2d33fa0786e31be41f924b8a7b93bb9d017a92c8
Davey Holler <daveyholler@hey.com>
2023-05-17 13:47:56 -0700

    Removing secondary branding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,16 +5,7 @@ export const Header = () => {
   return (
     <div className="inline-flex space-x-6 items-center justify-start px-8 py-3.5 bg-dark-ink w-full">
       <div className="flex space-x-1 items-center justify-end pr-8 border-r border-ink">
-        <img width={82} height={28} src={elasticTypeMark} />
-      </div>
-      <div className="flex space-x-3 items-center justify-start">
-        <img
-          className="w-8 h-8 rounded-lg"
-          src="https://via.placeholder.com/32x32"
-        />
-        <p className="text-xl font-extrabold leading-loose text-light-fog">
-          Acme Steel, Inc.
-        </p>
+        <img width={118} height={40} src={elasticTypeMark} />
       </div>
       <div className="flex flex-col items-center justify-center w-10 h-10 p-2 bg-ink rounded-full hidden">
         <p className="text-lg font-solid leading-normal text-white">sign-out</p>
